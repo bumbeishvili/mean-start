61adc165e83d345f8a077ff89230b39476cbfb36
Dato <davit.bumbeishvili@gmail.com>
2017-02-04 22:34:37 +0400

    apply reusable chart principle

--- client/app/components/pie/pie.component.js
+++ client/app/components/pie/pie.component.js
@@ -13,142 +13,140 @@ var d3 = require("../../../../node_modules/d3/build/d3.js");
 var task_services_1 = require('../../services/task.services');
 var PieComponent = (function () {
     function PieComponent(elementRef, renderer, taskService) {
+        this.elementRef = elementRef;
         this.renderer = renderer;
         this.taskService = taskService;
-        this.drew = false;
-        this.dom = document.body;
-        this.elementRef = elementRef;
+        this.chart = this.pieChart()
+            .width(300)
+            .height(300);
     }
-    PieComponent.prototype.onChanges = function () {
-        console.log('changed');
-    };
     PieComponent.prototype.ngOnInit = function () {
-        this.chartInit();
-    };
-    PieComponent.prototype.ngOnDestroy = function () {
-        this.renderer.setElementClass(this.dom, 'is-not-scrollable', false);
-    };
-    PieComponent.prototype.chartInit = function () {
         var _this = this;
-        var width, height;
-        var chartWidth, chartHeight;
-        var margin;
-        var svg = d3.select(this.elementRef.nativeElement)
+        d3.select(this.elementRef.nativeElement)
             .select("#pieChartContainer")
-            .append("svg");
-        var chartLayer = svg.append("g")
-            .classed("chartLayer", true);
-        var arc = d3.arc()
-            .padAngle(0.03)
-            .cornerRadius(8);
-        var pie = d3.pie()
-            .sort(null)
-            .value(function (d) { return d.value; });
-        // this.taskService
-        //   .getTasks()
-        //   .subscribe(tasks => {
-        //     console.log(tasks);
-        //     this.tasks = tasks;
-        //     var doneTasks = tasks.filter((t: Task) => t.isDone);
-        //     console.log(tasks);
-        //     main([{
-        //       name: "Done",
-        //       value: doneTasks.length
-        //     },
-        //     {
-        //       name: "Not Done",
-        //       value: tasks.length - doneTasks.length
-        //     }]);
-        //   });
+            .call(this.chart);
         this.taskService.tasksUpdated.subscribe(function (updatedTasks) {
-            console.log('task changed inside pie component');
-            _this.tasks = updatedTasks;
-            var doneTasks = _this.tasks.filter(function (t) { return t.isDone; });
-            console.log(_this.tasks);
+            var doneTasks = updatedTasks.filter(function (t) { return t.isDone; });
             var newData = [{
                     name: "Done",
                     value: doneTasks.length
-                },
-                {
+                }, {
                     name: "Not Done",
-                    value: _this.tasks.length - doneTasks.length
+                    value: updatedTasks.length - doneTasks.length
                 }];
-            if (_this.drew) {
-                console.log("minor");
-                redrawChart(newData);
-            }
-            else {
-                console.log("main");
-                main(newData);
-            }
-            _this.drew = true;
+            _this.chart.data(newData);
         });
-        function cast(d) {
-            d.value = +d.value;
-            return d;
-        }
-        function main(data) {
-            setSize(data);
-            drawChart(data);
-        }
-        function setSize(data) {
-            width = 300;
-            height = 300;
-            margin = { top: 40, left: 0, bottom: 40, right: 0 };
-            chartWidth = width - (margin.left + margin.right);
-            chartHeight = height - (margin.top + margin.bottom);
-            arc = arc
-                .outerRadius(chartHeight / 2)
-                .innerRadius(chartHeight / 4);
-            svg.attr("width", width).attr("height", height);
-            chartLayer
-                .attr("width", chartWidth)
-                .attr("height", chartHeight)
-                .attr("transform", "translate(" + [margin.left, margin.top] + ")");
-        }
-        function redrawChart(newData) {
-            debugger;
-            svg.selectAll('.arc')
-                .data(pie(newData))
-                .select('path')
-                .transition()
-                .duration(700)
-                .attrTween('d', arcTween);
-        }
-        function arcTween(a) {
-            debugger;
-            var i = d3.interpolate(this._current, a);
-            this._current = i(0);
-            return function (t) {
-                var result = arc(i(t));
-                console.log(result);
-                return result;
-            };
-        }
-        function drawChart(data) {
-            var arcs = pie(data);
-            var pieG = chartLayer.selectAll("g")
-                .data([data])
-                .enter()
-                .append("g")
-                .attr("transform", "translate(" + [chartWidth / 2, chartHeight / 2] + ")");
-            var block = pieG.selectAll(".arc")
-                .data(arcs);
-            var newBlock = block.enter().append("g").classed("arc", true);
-            newBlock.append("path")
-                .attr("d", arc)
-                .attr("id", function (d, i) { return "arc-" + i; })
-                .attr("stroke", "gray")
-                .attr("fill", function (d, i) { return d3.interpolateCool(Math.random()); })
-                .each(function (d) { this._current = d; });
-            newBlock.append("text")
-                .attr("dx", 55)
-                .attr("dy", -5)
-                .append("textPath")
-                .attr("xlink:href", function (d, i) { return "#arc-" + i; })
-                .text(function (d) { console.log(d); return d.data.name; });
-        }
     };
+    PieComponent.prototype.pieChart = function () {
+        // exposed variables
+        var attrs = {
+            width: null,
+            height: null,
+            chartWidth: null,
+            chartHeight: null,
+            margin: { top: 40, left: 0, bottom: 40, right: 0 },
+            data: [{ name: "Done", value: 0 }, { name: "Not Done", value: 2 }]
+        };
+        //innerFunctions
+        var updateData;
+        //main chart object
+        var chart = function (selection) {
+            selection.each(function () {
+                //calculated properties
+                var calc = {};
+                calc.chartWidth = attrs.width - (attrs.margin.left + attrs.margin.right);
+                calc.chartHeight = attrs.height - (attrs.margin.top + attrs.margin.bottom);
+                calc.chartInnerRadius = calc.chartHeight / 4;
+                calc.chartOuterRadius = calc.chartHeight / 2;
+                calc.chartHorizontalCenter = calc.chartWidth / 2;
+                calc.chartVerticalCenter = calc.chartHeight / 2;
+                calc.centerPointCoordinates = [calc.chartHorizontalCenter, calc.chartVerticalCenter];
+                var arc = d3.arc()
+                    .padAngle(0.03)
+                    .cornerRadius(8)
+                    .outerRadius(calc.chartOuterRadius)
+                    .innerRadius(calc.chartInnerRadius);
+                var pie = d3.pie()
+                    .sort(null)
+                    .value(function (d) { return d.value; });
+                var arcs = pie(attrs.data);
+                //drawing
+                var svg = d3.select(this)
+                    .append("svg")
+                    .attr("width", attrs.width)
+                    .attr("height", attrs.height);
+                var chartLayer = svg.append("g")
+                    .classed("chartLayer", true)
+                    .attr("width", calc.chartWidth)
+                    .attr("height", calc.chartHeight)
+                    .attr("transform", "translate(" + [attrs.margin.left, attrs.margin.top] + ")");
+                var pieG = chartLayer.selectAll("g")
+                    .data([attrs.data])
+                    .enter()
+                    .append("g")
+                    .attr("transform", "translate(" + calc.centerPointCoordinates + ")");
+                var block = pieG.selectAll(".arc")
+                    .data(arcs);
+                var newBlock = block.enter()
+                    .append("g")
+                    .classed("arc", true);
+                newBlock.append("path")
+                    .attr("d", arc)
+                    .attr("id", function (d, i) { return "arc-" + i; })
+                    .attr("stroke", "gray")
+                    .attr("fill", function (d, i) { return d3.interpolateCool(Math.random()); })
+                    .each(function (d) { this._current = d; });
+                newBlock.append("text")
+                    .attr("dx", 55)
+                    .attr("dy", -5)
+                    .append("textPath")
+                    .attr("xlink:href", function (d, i) { return "#arc-" + i; })
+                    .text(function (d) { console.log(d); return d.data.name; });
+                // smoothly handle data updating
+                updateData = function () {
+                    svg.selectAll('.arc')
+                        .data(pie(attrs.data))
+                        .select('path')
+                        .transition()
+                        .duration(700)
+                        .attrTween('d', arcTween);
+                };
+                function arcTween(a) {
+                    var i = d3.interpolate(this._current, a);
+                    this._current = i(0);
+                    return function (t) {
+                        var result = arc(i(t));
+                        console.log(result);
+                        return result;
+                    };
+                }
+            });
+        };
+        //exposed variables funcs
+        chart.data = function (value) {
+            if (!arguments.length)
+                return attrs.data;
+            attrs.data = value;
+            if (typeof updateData === 'function') {
+                updateData();
+            }
+            return chart;
+        };
+        chart.width = function (value) {
+            if (!arguments.length)
+                return attrs.width;
+            attrs.width = value;
+            return chart;
+        };
+        chart.height = function (value) {
+            if (!arguments.length)
+                return attrs.height;
+            attrs.height = value;
+            return chart;
+        };
+        return chart;
+    };
+    ;
     PieComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
--- client/app/components/pie/pie.component.ts
+++ client/app/components/pie/pie.component.ts
@@ -3,209 +3,199 @@ import * as d3 from "../../../../node_modules/d3/build/d3.js";
 import { TaskService } from '../../services/task.services';
 import { Task } from '../../../Task';
 
+interface IChart {
+  (): any,
+  data: (value: any[]) => any,
+  width: (value: number) => any,
+  height: (value: number) => any,
+}
+
+
 @Component({
   moduleId: module.id,
   selector: 'pie',
   templateUrl: 'pie.component.html'
 })
 
+
 export class PieComponent {
-  drew = false;
-  tasks: Task[];
-  dom: any = document.body;
-  elementRef: ElementRef;
+  chart: any;
 
-  constructor(elementRef: ElementRef, private renderer: Renderer, private taskService: TaskService) {
-    this.elementRef = elementRef;
-
+  constructor(private elementRef: ElementRef, private renderer: Renderer, private taskService: TaskService) {
+    this.chart = this.pieChart()
+      .width(300)
+      .height(300);
   }
 
-  onChanges() {
-    console.log('changed')
-  }
+
+
   ngOnInit() {
-    this.chartInit();
-  }
 
-  ngOnDestroy() {
-    this.renderer.setElementClass(this.dom, 'is-not-scrollable', false);
-  }
-
-  chartInit() {
-
-    var width, height
-    var chartWidth, chartHeight
-    var margin
-    var svg = d3.select(this.elementRef.nativeElement)
+    d3.select(this.elementRef.nativeElement)
       .select("#pieChartContainer")
-      .append("svg")
-
-
-
-
-    var chartLayer = svg.append("g")
-      .classed("chartLayer", true)
-
-    var arc = d3.arc()
-      .padAngle(0.03)
-      .cornerRadius(8)
-
-
-    var pie = d3.pie()
-      .sort(null)
-      .value(function (d) { return d.value; })
-
-    // this.taskService
-    //   .getTasks()
-    //   .subscribe(tasks => {
-    //     console.log(tasks);
-    //     this.tasks = tasks;
-
-    //     var doneTasks = tasks.filter((t: Task) => t.isDone);
-
-    //     console.log(tasks);
-
-    //     main([{
-    //       name: "Done",
-    //       value: doneTasks.length
-    //     },
-    //     {
-    //       name: "Not Done",
-    //       value: tasks.length - doneTasks.length
-    //     }]);
-
-    //   });
+      .call(this.chart);
 
     this.taskService.tasksUpdated.subscribe((updatedTasks: Task[]) => {
-      console.log('task changed inside pie component')
-
-      this.tasks = updatedTasks;
-
-      var doneTasks = this.tasks.filter((t: Task) => t.isDone);
-
-      console.log(this.tasks);
+      var doneTasks = updatedTasks.filter((t: Task) => t.isDone);
 
       var newData = [{
         name: "Done",
         value: doneTasks.length
-      },
-      {
+      }, {
         name: "Not Done",
-        value: this.tasks.length - doneTasks.length
+        value: updatedTasks.length - doneTasks.length
       }];
 
-
-      if (this.drew) {
-        console.log("minor");
-        redrawChart(newData);
-
-      } else {
-        console.log("main");
-        main(newData);
-      }
-
-
-      this.drew = true;
-
+      this.chart.data(newData);
     });
-
-
-
-
-
-
-    function cast(d) {
-      d.value = +d.value
-      return d
-    }
-
-    function main(data) {
-      setSize(data)
-      drawChart(data)
-    }
-
-    function setSize(data) {
-      width = 300
-      height = 300
-
-      margin = { top: 40, left: 0, bottom: 40, right: 0 }
-
-
-      chartWidth = width - (margin.left + margin.right)
-      chartHeight = height - (margin.top + margin.bottom)
-
-      arc = arc
-        .outerRadius(chartHeight / 2)
-        .innerRadius(chartHeight / 4)
-
-      svg.attr("width", width).attr("height", height);
-
-
-
-      chartLayer
-        .attr("width", chartWidth)
-        .attr("height", chartHeight)
-        .attr("transform", "translate(" + [margin.left, margin.top] + ")")
-
-
-    }
-    function redrawChart(newData: Task[]) {
-      debugger;
-      svg.selectAll('.arc')
-        .data(pie(newData))
-        .select('path')
-        .transition()
-        .duration(700)
-        .attrTween('d', arcTween)
-
-    }
-
-    function arcTween(a) {
-      debugger;
-      var i = d3.interpolate(this._current, a);
-      this._current = i(0);
-      return function (t) {
-        var result = arc(i(t));
-        console.log(result);
-        return result;
-      };
-    }
-
-
-    function drawChart(data) {
-
-
-
-      var arcs = pie(data);
-
-      var pieG = chartLayer.selectAll("g")
-        .data([data])
-        .enter()
-        .append("g")
-        .attr("transform", "translate(" + [chartWidth / 2, chartHeight / 2] + ")")
-
-      var block = pieG.selectAll(".arc")
-        .data(arcs)
-
-      var newBlock = block.enter().append("g").classed("arc", true)
-
-
-      newBlock.append("path")
-        .attr("d", arc)
-        .attr("id", function (d, i) { return "arc-" + i })
-        .attr("stroke", "gray")
-        .attr("fill", function (d, i) { return d3.interpolateCool(Math.random()) })
-        .each(function (d) { this._current = d; });
-
-
-      newBlock.append("text")
-        .attr("dx", 55)
-        .attr("dy", -5)
-        .append("textPath")
-        .attr("xlink:href", function (d, i) { return "#arc-" + i; })
-        .text(function (d) { console.log(d); return d.data.name })
-    }
   }
-}
+
+  pieChart() {
+    // exposed variables
+    var attrs: any = {
+      width: null,
+      height: null,
+      chartWidth: null,
+      chartHeight: null,
+      margin: { top: 40, left: 0, bottom: 40, right: 0 },
+      data: [{ name: "Done", value: 0 }, { name: "Not Done", value: 2 }]
+    };
+
+
+    //innerFunctions
+    var updateData: any;
+
+
+    //main chart object
+    var chart = <IChart>function (selection: any) {
+      selection.each(function () {
+
+        //calculated properties
+        var calc: any = {}
+        calc.chartWidth = attrs.width - (attrs.margin.left + attrs.margin.right)
+        calc.chartHeight = attrs.height - (attrs.margin.top + attrs.margin.bottom)
+        calc.chartInnerRadius = calc.chartHeight / 4;
+        calc.chartOuterRadius = calc.chartHeight / 2;
+        calc.chartHorizontalCenter = calc.chartWidth / 2;
+        calc.chartVerticalCenter = calc.chartHeight / 2;
+        calc.centerPointCoordinates = [calc.chartHorizontalCenter, calc.chartVerticalCenter];
+
+
+
+        var arc = d3.arc()
+          .padAngle(0.03)
+          .cornerRadius(8)
+          .outerRadius(calc.chartOuterRadius)
+          .innerRadius(calc.chartInnerRadius)
+
+
+        var pie = d3.pie()
+          .sort(null)
+          .value(function (d) { return d.value; })
+
+        var arcs = pie(attrs.data);
+
+        //drawing
+        var svg = d3.select(this)
+          .append("svg")
+          .attr("width", attrs.width)
+          .attr("height", attrs.height);
+
+
+        var chartLayer = svg.append("g")
+          .classed("chartLayer", true)
+          .attr("width", calc.chartWidth)
+          .attr("height", calc.chartHeight)
+          .attr("transform", "translate(" + [attrs.margin.left, attrs.margin.top] + ")")
+
+
+
+        var pieG = chartLayer.selectAll("g")
+          .data([attrs.data])
+          .enter()
+          .append("g")
+          .attr("transform", "translate(" + calc.centerPointCoordinates + ")")
+
+        var block = pieG.selectAll(".arc")
+          .data(arcs)
+
+        var newBlock = block.enter()
+          .append("g")
+          .classed("arc", true)
+
+
+        newBlock.append("path")
+          .attr("d", arc)
+          .attr("id", function (d, i) { return "arc-" + i })
+          .attr("stroke", "gray")
+          .attr("fill", function (d, i) { return d3.interpolateCool(Math.random()) })
+          .each(function (d) { this._current = d; });
+
+
+        newBlock.append("text")
+          .attr("dx", 55)
+          .attr("dy", -5)
+          .append("textPath")
+          .attr("xlink:href", function (d, i) { return "#arc-" + i; })
+          .text(function (d: any) { console.log(d); return d.data.name })
+
+
+        // smoothly handle data updating
+        updateData = function () {
+          svg.selectAll('.arc')
+            .data(pie(attrs.data))
+            .select('path')
+            .transition()
+            .duration(700)
+            .attrTween('d', arcTween)
+
+        }
+
+
+        function arcTween(a: any) {
+          var i = d3.interpolate(this._current, a);
+          this._current = i(0);
+          return function (t: any) {
+            var result = arc(i(t));
+            console.log(result);
+            return result;
+          };
+        }
+
+
+      });
+    }
+
+
+    //exposed variables funcs
+    chart.data = function (value: any[]) {
+      if (!arguments.length) return attrs.data;
+      attrs.data = value;
+      if (typeof updateData === 'function') {
+        updateData();
+      }
+      return chart;
+    }
+
+    chart.width = function (value: number) {
+      if (!arguments.length) return attrs.width;
+      attrs.width = value;
+      return chart;
+    }
+
+    chart.height = function (value: number) {
+      if (!arguments.length) return attrs.height;
+      attrs.height = value;
+      return chart;
+    }
+
+
+    return chart;
+  };
+
 
 }
+
+
+
+
